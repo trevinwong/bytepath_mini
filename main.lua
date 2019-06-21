00be40d523bdcc154065f89e210f3d7a3934b9b9
terbb <trevinwong@gmail.com>
2019-06-20 20:46:55 -0700

    Game Loop: Exercise 4

--- main.lua
+++ main.lua
@@ -1,10 +1,21 @@
 --[[
-  Exercise 3:
-    We return back to the original love.run() function which implements variable timestep.
-    In other words, we simply simulate as much state as the time passed since the last frame. 
+  Exercise 4:
+    Since we know that large dt can blow up our game, we want to cap it to some maximum.
+    This can give us the best of both worlds - allowing us to run at the correct speed on different machines while not being susceptible to blowing up.
     
-    There are some side-effects though: we are susceptible to extremely large changes of state if we are lagging really badly, which is not good, since our
-    dt will be massive. This can cause physics simulations to blow up.
+    How should we do this? If our dt is bigger than our desired frame rate (i.e 1/60), we split it into chunks that are max as big as our desired frame rate,
+    and simulate those chunks instead of simulating one huge chunk all at once.
+    
+    Here's the problem - we're now simulating multiple physics steps per display update. If our physics simulation is the most expensive part of rendering our frame,
+    we will run into the "spiral of death" problem.
+    
+    What is this problem and how is it caused exactly? Well, the CPU only has a certain amount of time to simulate the physics before the next frame. What happens if we
+    tell the CPU to simulate so many steps that it doesn't even have enough time to simulate all of them before the next frame? Thus the simulation falls behind.
+    We run into the "spiral of death" here because the CPU has to simulate more frames to catch up, which causes it to fall further behind, causing it to
+    simulate more frames, and so on.
+    
+    Some suggestions to avoid this: make sure your simulation runs very very fast, so it can handle temporary lag spikes where it needs to simulate a lot of frames,
+    or, cap the number of simulation steps per frame. The simulation will appear to slow down (we haven't simulated enough state!) but at least it will not die.
 --]]
 function love.run()
 	if love.load then love.load(love.arg.parseGameArguments(arg), arg) end
@@ -12,6 +23,7 @@ function love.run()
 	-- We don't want the first frame's dt to include time taken by love.load.
 	if love.timer then love.timer.step() end
  
+  local cap = 1/60
 	local dt = 0
  
 	-- Main loop time.
@@ -30,10 +42,15 @@ function love.run()
 		end
  
 		-- Update dt, as we'll be passing it to update
-		if love.timer then dt = love.timer.step() end
- 
+		if love.timer then dt = love.timer.step() end 
 		-- Call update and draw
-		if love.update then love.update(dt) end -- will pass 0 if love.timer is disabled
+		if love.update then 
+      while (dt > 0.0) do
+        local deltaTime = min(cap, dt)
+        love.update(deltaTime)
+        dt = dt - deltaTime
+      end
+    end -- will pass 0 if love.timer is disabled
  
 		if love.graphics and love.graphics.isActive() then
 			love.graphics.origin()
