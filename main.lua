976c7fe03cd32596da2bec2f880861bb5e604688
terbb <trevinwong@gmail.com>
2019-06-23 11:38:17 -0700

    Libraries: Exercise 36

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ function love.load()
   c = {'1', '2', '3', 4, 5, 6}
   d = {1, 4, 3, 4, 5, 6}
   
-  M.each(M.select(d, function(v) return v < 5 end), print)
+  M.each(M.select(c, function(v) return type(v) == 'string' end), print)
 end
 
 function love.update(dt)
