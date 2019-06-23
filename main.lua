7a5d342937823f3eb7ca7814f90f4e1ea9c066ca
terbb <trevinwong@gmail.com>
2019-06-22 20:18:44 -0700

    Libraries: Exercise 23

--- main.lua
+++ main.lua
@@ -8,16 +8,17 @@ function love.load()
   input = Input()
   timer = Timer()
   
-  rect_1 = {x = 400, y = 300, w = 50, h = 200}
+  rect_1 = {x = 400, y = 300, w = 200, h = 50}
   rect_2 = {x = 400, y = 300, w = 200, h = 50}
-  
-  timer:tween(1, rect_1, {w = 0}, 'in-out-cubic', 
-    function() timer:tween(1, rect_2, {h = 0}, 'in-out-cubic',
-        function() 
-          timer:tween(2, rect_1, {w = 50}, 'in-out-cubic')
-          timer:tween(2, rect_2, {h = 50}, 'in-out-cubic')
-        end
-      )
+
+  input:bind('d', function()
+      offset = (love.math.random() * 200) / 2
+      if rect_1.w - offset < 0 then offset = rect_1.w end
+      newWidth = rect_1.w - offset
+      newX = rect_1.x - (offset / 2)
+      temp = {w = newWidth, x = newX }
+      timer:tween(1, rect_1, temp, 'in-out-cubic')
+      timer:tween(0.5, rect_2, temp, 'in-out-cubic')
     end
   )
 end
@@ -27,7 +28,9 @@ function love.update(dt)
 end
 
 function love.draw()
+  love.graphics.setColor(0.8, 0.2, 0.2)
   love.graphics.rectangle('fill', rect_1.x - rect_1.w/2, rect_1.y - rect_1.h/2, rect_1.w, rect_1.h)
+  love.graphics.setColor(1.0, 0.4, 0.4)
   love.graphics.rectangle('fill', rect_2.x - rect_2.w/2, rect_2.y - rect_2.h/2, rect_2.w, rect_2.h)
 end
 
