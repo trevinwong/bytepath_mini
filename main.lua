a4f2d1a11eb95602db8b250ff624102db5b8fc68
terbb <trevinwong@gmail.com>
2019-06-23 10:06:20 -0700

    Libraries: Exercise 24

--- main.lua
+++ main.lua
@@ -8,19 +8,12 @@ function love.load()
   input = Input()
   timer = Timer()
   
-  rect_1 = {x = 400, y = 300, w = 200, h = 50}
-  rect_2 = {x = 400, y = 300, w = 200, h = 50}
-
-  input:bind('d', function()
-      offset = (love.math.random() * 200) / 2
-      if rect_1.w - offset < 0 then offset = rect_1.w end
-      newWidth = rect_1.w - offset
-      newX = rect_1.x - (offset / 2)
-      temp = {w = newWidth, x = newX }
-      timer:tween(1, rect_1, temp, 'in-out-cubic')
-      timer:tween(0.5, rect_2, temp, 'in-out-cubic')
-    end
-  )
+  circle = {radius = 24}
+  timer:every(4, function()
+      timer:tween(2, circle, {radius = 96}, 'in-out-cubic', function()
+          timer:tween(2, circle, {radius = 24}, 'in-out-cubic')
+      end)
+  end)
 end
 
 function love.update(dt)
@@ -28,10 +21,7 @@ function love.update(dt)
 end
 
 function love.draw()
-  love.graphics.setColor(0.8, 0.2, 0.2)
-  love.graphics.rectangle('fill', rect_1.x - rect_1.w/2, rect_1.y - rect_1.h/2, rect_1.w, rect_1.h)
-  love.graphics.setColor(1.0, 0.4, 0.4)
-  love.graphics.rectangle('fill', rect_2.x - rect_2.w/2, rect_2.y - rect_2.h/2, rect_2.w, rect_2.h)
+  love.graphics.circle('fill', 400, 300, circle.radius)
 end
 
 function requireAllInFolder(folder)
