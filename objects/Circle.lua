7487f258ecc2acfe4dfd94b221042bacdf104515
terbb <trevinwong@gmail.com>
2019-06-25 22:05:30 -0700

    Rooms and Areas: Exercise 49

--- objects/Circle.lua
+++ objects/Circle.lua
@@ -2,34 +2,20 @@ require "objects/GameObject"
 
 Circle = GameObject:extend()
 
---[[
-  Exercise 48:
-    OOP is a bit tricky if you don't know how it actually works in Lua. The way it's implemented is through metatables and the __index metamethod, which is incredibly
-    confusing if it's your first time.
-
-    The important thing to remember here is that we're always operating on "self" - but what is "self"? In Java, we assume it to be the object that we're creating, but
-    in Lua, this is actually the receiver of the operation. In new(), it'll be our new anonymous table.
-    
-    Hence, we can't actually call self.super() in new(), because super hasn't been assigned to our new anonymous table. Rather, it's assigned to our global object
-    (which we refer to as our class). So we have to call "Circle.super".
-    
-    The other gotcha is that we have to use the period notation here and pass in "self" instead of the colon notation - if we call Circle.super:new(), we're actually
-    passing in Circle.super as the "self" argument for new(), and we'll be assigning all the properties to our superclass' table. We have to make sure we're properly
-    passing in our anonymous table.
-    
-    So the below is the proper way to call your superclass' method.
-]]--
-
 function Circle:new(area, x, y, opts)
-  Circle.super.new(self, area, x, y, opts)
-  --[[
-    Exercise 48:
-      Remember how, in Exercise 25, we couldn't simulate the "every" functionality using the "after" functionality for timers, thanks to our usage of Chrono?
-      Well, it seems like the opposite has occured. hump.timer has no functionality for random delays, whereas Chrono does. The humanity.
-      
-      In the near future, I may look into fixing this for Chrono/EnhancedTimer. For now, we're using a fixed delay.
-  ]]--
+  local opts = opts or {}
+  if opts then for k, v in pairs(opts) do self[k] = v end end
+
+  self.area = area
+  self.x, self.y = x, y
+  self.id = UUID()
+  self.dead = false
+  self.timer = Timer()
   self.timer:after(4, function() self.dead = true end)
+end
+
+function Circle:update(dt)
+    if self.timer then self.timer:update(dt) end
 end
 
 function Circle:draw()
--- rooms/Stage.lua
+++ rooms/Stage.lua
@@ -1,17 +1,35 @@
 Stage = Object:extend()
 
+--[[
+    Exercise 49:
+      To be honest, I'm not sure why this exercise is here, considering all we have to do is paste the exact same code that we get from abstracting our code into          
+      "GameObject" and "Area". Perhaps it's to illustrate why abstracting those concepts saves us a lot of time and headache. Indeed, all this code would be way
+      too annoying to re-write every time.
+]]--
+
 function Stage:new()
-  self.area = Area(self)
   self.timer = Timer()
+  self.game_objects = {}
   local radius = 20
-  self.timer:every(2, function() self.area:addGameObject('Circle', love.math.random() * window.width, love.math.random() * window.height, { radius = radius }) end)
+  self.timer:every(2, function() self:addGameObject('Circle', love.math.random() * window.width, love.math.random() * window.height, { radius = radius }) end)
 end
 
 function Stage:update(dt)
   self.timer:update(dt)
-  self.area:update(dt)
+  for i = #self.game_objects, 1, -1 do
+      local game_object = self.game_objects[i]
+      game_object:update(dt)
+      if game_object.dead then table.remove(self.game_objects, i) end
+  end
 end
 
 function Stage:draw()
-  self.area:draw()
+  for _, game_object in ipairs(self.game_objects) do game_object:draw() end
+end
+
+function Stage:addGameObject(game_object_type, x, y, opts)
+    local opts = opts or {}
+    local game_object = _G[game_object_type](self, x or 0, y or 0, opts)
+    table.insert(self.game_objects, game_object)
+    return game_object
 end
