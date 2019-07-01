8bb43809b3c451e9bb5c9f7b09a44b9eb7489375
terbb <trevinwong@gmail.com>
2019-07-01 13:51:23 -0700

    Game Basics: Exercise 71

--- rooms/Stage.lua
+++ rooms/Stage.lua
@@ -28,4 +28,23 @@ function Stage:draw()
   love.graphics.setBlendMode('alpha', 'premultiplied')
   love.graphics.draw(self.main_canvas, 0, 0, 0, sx, sy)
   love.graphics.setBlendMode('alpha')
+  
+--[[
+  Exercise 71:
+    Sadly, love.graphics.setPointStyle() was deprecated after version 10.0, so I couldn't perfectly replicate the image.
+    
+    The logic behind this is pretty simple: the angle gives us the direction of where our line should point. If we want to "translate" this into x and y, we use
+    cos and sin. Since they only give us the direction, we need to multiply it by the actual length of our line. Finally, we shift our point of origin to
+    pointA by adding the coordinates of pointA.
+]]--
+
+  local pointA = {x = 300, y = 300}
+  local angle = -math.pi/4
+  local distance = 100
+  local pointB = {x = (math.cos(angle) * 100) + pointA.x, y = (math.sin(angle) * 100) + pointA.y}
+  love.graphics.setPointSize(5)
+  love.graphics.points(pointA.x, pointA.y, pointB.x, pointB.y)
+  love.graphics.print("A", pointA.x, pointA.y)
+  love.graphics.print("B", pointB.x, pointB.y)
+  love.graphics.line(pointA.x, pointA.y, pointB.x, pointB.y)
 end
