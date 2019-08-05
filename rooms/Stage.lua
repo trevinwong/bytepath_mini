8c108ef572681ebc841e2bd9b38a8cd613524723
terbb <trevinwong@gmail.com>
2019-08-05 15:31:02 -0700

    Director and Gameplay Loop: Exercise 120

--- rooms/Stage.lua
+++ rooms/Stage.lua
@@ -17,6 +17,7 @@ function Stage:new()
     
     self.director = self.area:addGameObject('Director', 0, 0)
     self.score = 0
+    self.font = fonts.m5x7_16
     
     input:bind('p', function() 
         self.area:addGameObject('Ammo', random(0, gw), random(0, gh)) 
@@ -39,6 +40,9 @@ function Stage:new()
     input:bind('r', function()
         self.area:addGameObject('Shooter')
     end)
+    input:bind('e', function()
+        self.score = self.score * 10
+    end)
 end
 
 function Stage:update(dt)
@@ -49,17 +53,62 @@ function Stage:update(dt)
 end
 
 function Stage:draw()
-  love.graphics.setCanvas(self.main_canvas)
-  love.graphics.clear()
+    love.graphics.setCanvas(self.main_canvas)
+    love.graphics.clear()
     camera:attach(0, 0, gw, gh)
       self.area:draw()
     camera:detach()
-  love.graphics.setCanvas()
+    
+    -- HP
+    local r, g, b = unpack(hp_color)
+    local hp, max_hp = self.player.hp, self.player.max_hp
+    love.graphics.setColor(r, g, b)
+    love.graphics.rectangle('fill', gw/2 - 52, gh - 16, 48*(hp/max_hp), 4)
+    love.graphics.setColor(r - 32/255, g - 32/255, b - 32/255)
+    love.graphics.rectangle('line', gw/2 - 52, gh - 16, 48, 4)
+    
+    love.graphics.print('HP', gw/2 - 52 + 24, gh - 24, 0, 1, 1,
+    math.floor(self.font:getWidth('HP')/2), math.floor(self.font:getHeight()/2))
+    
+    love.graphics.print(hp .. '/' .. max_hp, gw/2 - 52 + 24, gh - 6, 0, 1, 1,
+    math.floor(self.font:getWidth(hp .. '/' .. max_hp)/2), math.floor(self.font:getHeight()/2))
 
-  love.graphics.setColor(255, 255, 255, 255)
-  love.graphics.setBlendMode('alpha', 'premultiplied')
-  love.graphics.draw(self.main_canvas, 0, 0, 0, sx, sy)
-  love.graphics.setBlendMode('alpha')
+    -- Ammo
+    local r, g, b = unpack(ammo_color)
+    local ammo, max_ammo = self.player.ammo, self.player.max_ammo
+    love.graphics.setColor(r, g, b)
+    love.graphics.rectangle('fill', gw/2 - 52, 16, 48*(ammo/max_ammo), 4)
+    love.graphics.setColor(r - 32/255, g - 32/255, b - 32/255)
+    love.graphics.rectangle('line', gw/2 - 52, 16, 48, 4)
+    
+    love.graphics.print('AMMO', gw/2 - 52 + 24, 8, 0, 1, 1,
+    math.floor(self.font:getWidth('AMMO')/2), math.floor(self.font:getHeight()/2))
+    
+    love.graphics.print(ammo .. '/' .. max_ammo, gw/2 - 52 + 24, 26, 0, 1, 1,
+    math.floor(self.font:getWidth(ammo .. '/' .. max_ammo)/2), math.floor(self.font:getHeight()/2))
+    
+    -- Score
+    love.graphics.setColor(default_color)
+    love.graphics.print(self.score, gw - 40, 10, 0, 1, 1,
+    math.floor(self.font:getWidth(self.score)/1.5), self.font:getHeight()/2)
+    love.graphics.setColor(255, 255, 255)
+    
+    -- SP
+    love.graphics.setColor(skill_point_color)
+    love.graphics.print(sp .. "SP", 20, 10, 0, 1, 1,
+    0, self.font:getHeight()/2)
+    love.graphics.setColor(255, 255, 255)
+    
+    love.graphics.setCanvas()
+
+    love.graphics.setColor(255, 255, 255, 255)
+    love.graphics.setBlendMode('alpha', 'premultiplied')
+    love.graphics.draw(self.main_canvas, 0, 0, 0, sx, sy)
+    love.graphics.setBlendMode('alpha')
+
+    love.graphics.setFont(self.font)
+
+
 end
 
 function Stage:finish()
