c938e62e65c9f0d9c5ebc088f2bfc66468742477
terbb <trevinwong@gmail.com>
2019-08-05 11:30:28 -0700

    Director and Gameplay Loop: Exercise 118

--- objects/Director.lua
+++ objects/Director.lua
@@ -83,6 +83,20 @@ end
 function Director:setEnemySpawnsForThisRound()
     local points = self.difficulty_to_points[self.difficulty]
 
+	--[[
+		Exercise 118:
+			Interestingly enough, the existing code already handles this case. The author claims that the while loop will get stuck if we can only spawn
+			enemies with point values higher than the number of points we currently have to allocate for enemies.
+			
+			This is actually not the case however, since we don't actually care how many points we have remaining since our number of points can be negative
+			due to the fact that we do a simple subtraction. So even if we only have 1 point remaining, we can actually just spawn an enemy with an extremely
+			high point value, and that'll exit the loop since our number of points will be lower or equal to 0.
+			
+			If, however, the case that the author claims would happen were to actually happen, I would simply just spawn the lowest value enemy for that wave
+			and call it a day. But to be honest, I don't think the above scenario is actually a bad alternative (technically our player could get a little
+			screwed over by RNG), but since it's just 1 enemy, I don't think it will influence the gameplay that much.
+	]]--
+
     -- Find enemies
     local enemy_list = {}
     while points > 0 do
